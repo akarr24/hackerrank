98e9cbef050832d7599d87650102e8776cb79bc6
Aditya Karlekar <akarleker94@gmail.com>
2019-07-27 00:13:17 +0530

    Add files via upload

---
+++ Data Structures/reversing_doubly_linked_list.py
@@ -0,0 +1,93 @@
+#reversing_doubly_linked_list
+# -*- coding: utf-8 -*-
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the reverse function below.
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+def reverse(head):
+    if head is None:
+        return None
+    current = head
+    previous = None
+    while current is not None:
+        next = current.next
+        current.next = previous
+        current.previous = next
+        previous = current
+        current = next
+    
+    head = previous
+
+    return head
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
+
+
